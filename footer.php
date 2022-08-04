68395b72b40305f6c52200cb1ea70fbb1c73041b
flashgrinder <flashgrinder92@gmail.com>
2022-08-04 04:14:58 +0400

    make theme dis forum

---
+++ footer.php
@@ -0,0 +1,97 @@
+    </main>
+    <!-- /. MAIN -->
+
+    <!-- Footer -->
+    <footer class="footer gs-reveal">
+        <div class="footer__body container">
+            <div class="footer__inner">
+                <div class="footer__column">
+                    <h4 class="footer__col-title title title--pre-medium title--white title--w-semibold">
+                        О нас
+                    </h4>
+                    <p class="footer__descr text text--normal text--white text--w-regular">
+                        Решения по управлению данными, бизнес-аналитике, защите информации и управлению знаниями. Мы упрощаем работу с данными и информацией, предоставляя новые возможности для роста бизнеса.
+                    </p>
+                    <div class="footer__social">
+                        <a href="https://www.facebook.com/DIS-Group-141143739295193/" class="footer__soc-link link">
+                            Facebook
+                        </a>
+                        <a href="https://www.youtube.com/channel/UCfNXWK4lsMs6-Y5nS_Kf0VA" class="footer__soc-link link">
+                            Youtube
+                        </a>
+                    </div>
+                </div>
+                <div class="footer__column">
+                    <h4 class="footer__col-title title title--pre-medium title--white title--w-semibold">
+                        О мероприятии
+                    </h4>
+                    <div class="footer__info">
+                        <span class="footer__info-text text text--normal text--white text--w-light">
+                            Дата проведения:
+                        </span>
+                        <span class="footer__info-text text text--normal text--white text--w-semibold">
+                            22 сентября 2022
+                        </span>
+                    </div>
+                    <div class="footer__info">
+                        <span class="footer__info-text text text--normal text--white text--w-light">
+                            Время проведения:
+                        </span>
+                        <span class="footer__info-text text text--normal text--white text--w-semibold">
+                            10:00 - 18:00
+                        </span>
+                    </div>
+                </div>
+                <div class="footer__column">
+                    <h4 class="footer__col-title title title--pre-medium title--white title--w-semibold">
+                        Наши контакты
+                    </h4>
+                    <a href="tel:++74956450201" class="footer__info">
+                        <span class="footer__info-text text text--normal text--white text--w-light">
+                            Телефон:
+                        </span>
+                        <span class="footer__info-text text text--normal text--white text--w-semibold link">
+                            +7 495 645-0201
+                        </span>
+                    </a>
+                    <div class="footer__info">
+                        <span class="footer__info-text text text--normal text--white text--w-light">
+                            Факс:
+                        </span>
+                        <span class="footer__info-text text text--normal text--white text--w-semibold">
+                            +7 495 645-0188
+                        </span>
+                    </div>
+                    <a href="mailto:infaday@dis-group.ru" class="footer__info">
+                        <span class="footer__info-text text text--normal text--white text--w-light">
+                            E-mail:
+                        </span>
+                        <span class="footer__info-text text text--normal text--white text--w-semibold link">
+                            infaday@dis-group.ru
+                        </span>
+                    </a>
+                    <a href="<?php echo home_url(); ?>" class="footer__logo logo">
+                        <img src="<?php echo STANDART_DIR; ?>img/logo.svg" alt="" class="footer__logo-img logo__img">
+                    </a>
+                </div>
+            </div>
+            <div class="footer__decor">
+                <svg class="footer__decor-svg" width="221" height="74" viewBox="0 0 221 74" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <line x1="4" y1="70" x2="122" y2="70" stroke="#00E0FF" stroke-width="8" stroke-linecap="round"/>
+                    <line x1="141" y1="2" x2="219" y2="2" stroke="#8C3FFF" stroke-width="4" stroke-linecap="round"/>
+                </svg>
+            </div>
+        </div>
+        <div class="footer__bottom">
+            <div class="footer__copy container">
+                <div class="footer__copy-text text text--tiny text--white text--w-light">
+                    © Copyright 2022 DIS Group. All Rights Reserved.
+                </div>
+            </div>
+        </div>
+    </footer>
+    <!-- /. Footer -->
+
+    <?php wp_footer(); ?>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+
+<head>
+    <meta charset="<?php bloginfo('charset'); ?>"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no" />
+    <? wp_head(); ?>
+</head>
+
+<body class="page bg--white">
+
+    <!-- Preloader -->
+    <div class="page-preloader">
+        <div class="page-preloader__pulse"></div>
+    </div>
+    <!-- /. Preloader -->
+
+    <!-- Modal -->
+    <div class="modal hystmodal" id="registration" aria-hidden="true">
+        <div class="hystmodal__wrap">
+            <div class="modal__body hystmodal__window" role="modal window contact" aria-modal="true">
+                <div class="modal__close" data-hystclose>
+                    <svg class="modal__close-svg" viewBox="0 0 13 13" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M1 1L12 12M12 1L1 12" stroke-width="1.5" stroke-linecap="round"/>
+                    </svg>
+                </div>
+                <h3 class="modal__title modal__field--anim title title--large title--dark title--w-regular center">
+                    Регистрация
+                </h3>
+                <?php echo do_shortcode( '[contact-form-7 title="Регистрация" html_class="modal__form"]' ); ?>
+            </div>
+        </div>
+    </div>
+    <!-- /. Modal -->
+
+    <!-- Header -->
+    <header class="header">
+        <div class="header__body container">
+            <a href="<?php echo home_url(); ?>" class="header__logo logo">
+                <img src="<?php echo STANDART_DIR; ?>img/logo.svg" alt="" class="header__logo-img logo__img">
+            </a>
+            <div class="header__inner js-menu">
+                <nav class="header__menu menu">
+                    <ul class="header__menu-list menu__list">
+                        <li class="header__menu-item menu__item js-anchor-link">
+                            <a href="#event" class="menu__link">
+                                О мероприятии
+                            </a>
+                        </li>
+                        <li class="header__menu-item menu__item js-anchor-link">
+                            <a href="#discussions" class="menu__link">
+                                Дискуссии
+                            </a>
+                        </li>
+                        <li class="header__menu-item menu__item js-anchor-link">
+                            <a href="#programm" class="menu__link">
+                                Программа
+                            </a>
+                        </li>
+                        <li class="header__menu-item menu__item js-anchor-link">
+                            <a href="#partners" class="menu__link">
+                                Партнёры
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
+                <div class="header__action hide">
+                    <a href="javascript:;" class="header__button button button--primary" data-modal="#modal">
+                        Смотреть в записи
+                    </a>
+                </div>
+            </div>
+            <div class="header__burger-menu burger-menu js-burger">
+                <span class="burger-menu__line"></span>
+            </div>
+        </div>
+    </header>
+    <!-- /. Header -->
+
+    <!-- MAIN -->
+    <main class="main page__container">
